631af1db833417827d69138e8cc3ca9e2938ca07
Emma Nguyen <emma.mphuong@gmail.com>
2018-09-12 14:30:20 -0400

    Update training.py with BatchTrainer

---
+++ pytorch_deep_learning/training.py
@@ -0,0 +1,11 @@
+class BatchTrainer:
+    def __init__(self):
+        None
+    def __call__(self):
+        None
+    def train_on_batch(self):
+        None
+    def test_on_batch(self):
+        None
+    def eval_on_batch(self):
+        None
