2e81a278fa9546095b70abf4f513a6e4c15873b3
Emma Nguyen <emma.mphuong@gmail.com>
2018-09-14 00:53:52 -0400

    Add a unittest of module data loader via upload

---
+++ test/test_data_loader.py
@@ -0,0 +1,16 @@
+import unittest
+
+from pytorch_deep_learning.utils import data_loader
+
+class TestDataLoader(unittest.TestCase):
+
+    def test_Shepard_Metzler(self):
+        data_path = "/data/milatmp1/nguthien/emma/EmmaLab/DATA/3D_Scenes/Demo/shepard_metzler_7_parts-torch/train"
+
+        shepard_metzler = data_loader.ShepardMetzler(data_path)
+        self.assertEqual(shepard_metzler.__len__(), 1711)
+
+
+
+if __name__=="__main__":
+    unittest.main()
