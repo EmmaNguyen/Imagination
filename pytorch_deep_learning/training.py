cc2f06cea17efc81edb1ef39cd56eceec52ee282
Emma Nguyen <emma.mphuong@gmail.com>
2018-09-15 16:42:05 -0400

    Changes in design to help BatchTrainer and ModelTrainer run appropriately

--- pytorch_deep_learning/training.py
+++ pytorch_deep_learning/training.py
@@ -22,38 +22,46 @@ class BatchTrainer:
         return self.eval_on_batch(images, viewpoints)
 
     def train_on_batch(self, images, viewpoints):
-        self.optimizer.zero_grad() #reset gradients
         evals = self.eval_on_batch(images, viewpoints)
-        evals['evidence_lower_bound'].backward()
+        # import pdb; pdb.set_trace()
+        evals['evidence_lower_bound'].sum().backward()
         self.optimizer.step()
+        self.optimizer.zero_grad() #reset gradients
         return evals
 
     def test_on_batch(self, images, viewpoints):
-        images, viewpoints = images.to(self._device), viewpoints.to(self.viewpoints)
+        images, viewpoints = images.to(self._device), viewpoints.to(self._device)
         self.optimizer.zero_grad() #reset gradients
         return self.eval_on_batch(images, viewpoints)
 
     def eval_on_batch(self, images, viewpoints):
-        images, viewpoints = images.to(self._device), viewpoints.to(self.viewpoints)
-        reconstructions, query_viewpoints, representation, kl_div = self.model_(images, viewpoints)
-        negative_log = -Normal(reconstructions, sigma).log_prob(query_viewpoints)
+        images, viewpoints = images.to(self._device), viewpoints.to(self._device)
+        reconstructions, query_viewpoints, representation, kl_div = self.model(images, viewpoints)
+        # If more than one GPU we must take new shape into account
+        batch_size = query_viewpoints.size(0)
+
+        negative_log = -Normal(reconstructions, self.sigma).log_prob(query_viewpoints)
+
+        reconstruction = torch.mean(negative_log.view(batch_size, -1), dim=0).sum()
+        kl_div  = torch.mean(kl_div.view(batch_size, -1), dim=0).sum()
+
         evidence_lower_bound = negative_log + kl_div
         return {'evidence_lower_bound': evidence_lower_bound,
                 'kl_divergence': kl_div,
                 'negative_log': negative_log,
-                'reconstructions': reconstructions,
+                'reconstruction': reconstructions,
                 'viewpoints': viewpoints,
                 'representation': representation}
 
 class ModelTrainer(BatchTrainer):
 
-    def __init__(self, model, optimizer, device, dataset, mu=mu_f, sigma=sigma_f):
-        super(ModelTrainer, self).__init__()
+    def __init__(self, model, dataset, device, mu, sigma):
+        # super(ModelTrainer, self).__init__()
         self.model = model.to(device)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.mu)
         self._device = device
-        self.data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
+        self.data_loader = DataLoader(dataset, batch_size=48, shuffle=True)
         self.mu, self.sigma = mu, sigma
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.mu)
 
     def should_save_image(self, step, max_gradient_steps, periodic_step=1000):
         return step % periodic_step == 0
@@ -70,17 +78,18 @@ class ModelTrainer(BatchTrainer):
         self.sigma = max(sigma_f + (sigma_i - sigma_f)*(1 - step/(2 * 10**5)), sigma_f)
 
     def train(self, max_gradient_steps):
-        for step in range(max_gradient_steps):
+        for step in range(1, max_gradient_steps+1):
             for image_batch, viewpoint_batch in tqdm(self.data_loader):
-                batch_eval = self.train_on_batch(image_batch, viewpoint_batch)
-                if should_save_checkpoint(step, max_gradient_steps):
+                eval_batch = self.train_on_batch(image_batch, viewpoint_batch)
+                if self.should_save_checkpoint(step, max_gradient_steps):
                     torch.save(self.model, "model-{}-{}.pt".format(step,"0T00T00"))
 
             with torch.no_grad():
                 test_image, test_pointview = next(iter(self.data_loader))
-                test_reconstruction, test_representation = self.test_on_batch(test_image, test_pointview)['reconstructions', 'representation']
-                test_representation = test_representation.view(-1, 1, 16, 16)
-                if should_save_image(step, max_gradient_steps):
+                test_eval = self.test_on_batch(test_image, test_pointview)
+                test_reconstruction = test_eval['reconstruction']
+                test_representation = test_eval['representation'].view(-1, 1, 16, 16)
+                if self.should_save_image(step, max_gradient_steps):
                     save_image(test_reconstruction.float(), "reconstruction.jpg")
                     save_image(test_representation.float(), "representation.jpg")
 
