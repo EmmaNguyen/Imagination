774880ea56c1f1ab33a2944388bcd3d1578692ac
Emma Nguyen <emma.mphuong@gmail.com>
2018-09-15 16:39:58 -0400

    Import more modules and libraries (and some hot fix) to help class ModelTrainer run in v01

--- pytorch_deep_learning/models/representation_architectures.py
+++ pytorch_deep_learning/models/representation_architectures.py
@@ -3,56 +3,71 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TowerConv8D(nn.Module):
-    def __init__(self, in_channels, view_dim, rep_dim=256, pool=True):
+    def __init__(self, n_channels, v_dim, r_dim=256, pool=True):
+        """
+        Network that generates a condensed representation
+        vector from a joint input of image and viewpoint.
+
+        Employs the tower/pool architecture described in the paper.
+
+        :param n_channels: number of color channels in input image
+        :param v_dim: dimensions of the viewpoint vector
+        :param r_dim: dimensions of representation
+        :param pool: whether to pool representation
+        """
         super(TowerConv8D, self).__init__()
-        self.rep_dim = out_channels = rep_dim
+        # Final representation size
+        self.r_dim = k = r_dim
         self.pool = pool
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=2)
-        self.conv3 = nn.Conv2d(out_channels, out_channels//2, \
-            kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(out_channels//2, out_channels, \
-            kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(n_channels, k, kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(k, k, kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(k, k//2, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(k//2, k, kernel_size=2, stride=2)
 
-        self.conv5 = nn.Conv2d(out_channels + view_dim, out_channels, kernel_size=3, \
-            stride=1, padding=1)
-        self.conv6 = nn.Conv2d(out_channels + view_dim, out_channels//2, kernel_size=3, \
-            stride=1, padding=1)
-        self.conv7 = nn.Conv2d(out_channels//2, out_channels, kernel_size=3, \
-            stride=1, padding=1)
-        self.conv8 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1)
+        self.conv5 = nn.Conv2d(k + v_dim, k, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(k + v_dim, k//2, kernel_size=3, stride=1, padding=1)
+        self.conv7 = nn.Conv2d(k//2, k, kernel_size=3, stride=1, padding=1)
+        self.conv8 = nn.Conv2d(k, k, kernel_size=1, stride=1)
 
-        self.avgpool = nn.AvgPool2d(out_channels//16)
+        self.avgpool  = nn.AvgPool2d(k//16)
 
-    def forward(self, image, viewpoint):
-        # Increase dimension
-        view = view.view(view.size(0), -1, 1, 1)
-        view = view.repeat(1, 1, self.rep_dim//16, self.rep_dim//16)
+    def forward(self, x, v):
+        """
+        Send an (image, viewpoint) pair into the
+        network to generate a representation
+        :param x: image
+        :param v: viewpoint (x, y, z, cos(yaw), sin(yaw), cos(pitch), sin(pitch))
+        :return: representation
+        """
+        # Increase dimensions
+        v = v.view(v.size(0), -1, 1, 1)
+        v = v.repeat(1, 1, self.r_dim // 16, self.r_dim // 16)
 
         # First skip-connected conv block
-        skip_in = F.relu(self.conv1(image))
+        skip_in  = F.relu(self.conv1(x))
         skip_out = F.relu(self.conv2(skip_in))
 
-        image = F.relu(self.conv3(skip_in))
-        image = F.relu(self.conv4(image)) + skip_out
+        x = F.relu(self.conv3(skip_in))
+        x = F.relu(self.conv4(x)) + skip_out
 
         # Second skip-connected conv block (merged)
         skip_in = torch.cat([x, v], dim=1)
-        skip_out = F.relu(self.conv5(skip_in))
+        skip_out  = F.relu(self.conv5(skip_in))
 
-        image = F.relu(self.conv6(skip_in))
-        image = F.relu(self.conv7(image)) + skip_out
+        x = F.relu(self.conv6(skip_in))
+        x = F.relu(self.conv7(x)) + skip_out
 
-        rep = F.relu(self.conv8(image))
+        r = F.relu(self.conv8(x))
 
         if self.pool:
-            return self.avgpool(rep)
-        else:
-            return rep
+            r = self.avgpool(r)
+
+        return r
+
 
 class PyramidConv8D:
-    def __init__(self, n_channels, v_dim, r_dim=256):
+    def __init__(self, n_channels, view_dim, rep_dim=256):
         """
         Network that generates a condensed representation
         vector from a joint input of image and viewpoint.
@@ -60,14 +75,14 @@ class PyramidConv8D:
         Employs the pyramid architecture described in the paper.
 
         :param n_channels: number of color channels in input image
-        :param v_dim: dimensions of the viewpoint vector
-        :param r_dim: dimensions of representation
+        :param view_dim: dimensions of the viewpoint vector
+        :param rep_dim: dimensions of representation
         """
         super(PyramidRepresentation, self).__init__()
         # Final representation size
-        self.r_dim = k = r_dim
+        self.rep_dim = k = rep_dim
 
-        self.conv1 = nn.Conv2d(n_channels + v_dim, k//8, kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(n_channels + view_dim, k//8, kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(k//8, k//4, kernel_size=2, stride=2)
         self.conv3 = nn.Conv2d(k//4, k//2, kernel_size=2, stride=2)
         self.conv4 = nn.Conv2d(k//2, k, kernel_size=8, stride=8)
