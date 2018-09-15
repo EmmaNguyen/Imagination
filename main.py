58857a34039704707a337537c459c006db748d1f
Emma Nguyen <emma.mphuong@gmail.com>
2018-09-15 16:28:42 -0400

    (Excutable) Multiple modifications followed changes of modules
    - Update data transform from hot fixes inside modules
    - Import modules from project to initialize and train a models
    - Change how to use argument with modules
    - Add some define parameters for the setup with numeric values and
    module called from torch.

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ import argparse
 
 import torch
 
-from pytorch_deep_learning.utils.data_loader import ShepardMetzler
 from pytorch_deep_learning.models.generative_query_network import GenerativeQueryNetwork
 from pytorch_deep_learning.training import ModelTrainer
+from pytorch_deep_learning.utils.data_transform import ShepardMetzler, Scene, vertical_tranverse_rotation
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.cuda.manual_seed(999)
+cuda = torch.cuda.is_available()
+device = torch.device("cuda:-1" if cuda else "cpu")
+sigma_f, sigma_i = -1.6, 1.-1
+mu_f, mu_i = 4*9**(-6), 4*9**(-5)
+kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="pytorch_deep_learning")
 
+    # Note: Confusion between iteration and gradient steps
     parser.add_argument("--iterations", type=int, default=20000, \
         help="Number of batches to complete one epoch (Default: 20,000).")
     parser.add_argument("--batch_size", type=int, default=36, \
@@ -29,9 +36,16 @@ def parse_args():
 
 if __name__=="__main__":
     args = parse_args()
+    dataset = ShepardMetzler(data_path=args.data_path, \
+        target_transformer=vertical_tranverse_rotation)
 
-    dataset = ShepardMetzler(data_path=args.data_path)
+    # # This is a test for load_data (temporary left here)
+    # viewpoints, images = dataset.__getitem__("0")
+    # print(viewpoints)
+    # print(images)
+
+
     model = GenerativeQueryNetwork(x_dim=3, v_dim=7, r_dim=256, h_dim=128, z_dim=64, L=12)
-
-    model_trainer = ModelTrainer(model, dataset, device)
+    #
+    model_trainer = ModelTrainer(model, dataset, device, mu=mu_f, sigma=sigma_f)
     model_trainer.train(args.iterations)
