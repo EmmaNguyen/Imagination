72dd5c37df2908abc9828339b0862d54902dae62
Emma Nguyen <emma.mphuong@gmail.com>
2018-09-17 00:49:54 -0400

    Add a function to download data

--- tools/download_data.py
+++ tools/download_data.py
@@ -9,8 +9,15 @@ def parse_args():
 
     return parser.parse_args()
 
-def download_data():
-    pass
+def download_data(data_path):
+    file_name = "torch_small_shepard_metzler.tar"
+    download_url="https://www.dropbox.com/s/fc16aeeo0014dv2/{}".format(file_name)
+    test_link = "https://www.dropbox.com/s/fc16aeeo0014dv2/torch_small_shepard_metzler.tar?dl=0"
+    try:
+        import wget
+        wget.download(test_link, out=".")
+    except:
+        print("Require to install package: try `conda install wget`. More information at https://pypi.org/project/wget/")
 
 if __name__=="__main__":
     args = parse_args()
