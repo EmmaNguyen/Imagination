bfdac4d5a9ff214a278fe1bbf3a396e06f0aa82c
cirrascale <emma.mphuong@gmail.com.com>
2018-12-21 13:21:19 -0800

    Update filepath to set up Anaconda

--- scripts/download_and_setup_anaconda.sh
+++ scripts/download_and_setup_anaconda.sh
@@ -10,11 +10,11 @@ bash Anaconda3-4.2.0-Linux-x86_64.sh -b -p ./third_party/anaconda
 rm Anaconda3-4.2.0-Linux-x86_64.sh
 
 # Refresh bashrc for using this login
-echo 'export PATH="./Imagination/third_party/anaconda/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="./third_party/anaconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 # Create a PATH to a virtual environment under this project for next time
-echo 'export PATH="./Imagination/third_party/anaconda/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="./third_party/anaconda/bin:$PATH"' >> ~/.bash_profile
 
 # Get the latest version
 conda update conda --yes
